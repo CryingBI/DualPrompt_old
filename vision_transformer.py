bcf4d7ffecfffec5aa3a1c6c4bd94f3974711392
CryingBI <quanganhvp32401@gmail.com>
2023-07-06 17:58:22 +0700

    u

--- vision_transformer.py
+++ vision_transformer.py
@@ -467,14 +467,14 @@ class VisionTransformer(nn.Module):
         self.norm = norm_layer(embed_dim) if not use_fc_norm else nn.Identity()
 
         # Classifier Head
-        self.fc_norm = norm_layer(embed_dim) if use_fc_norm else nn.Identity()
-        self.head = nn.Sequential(
-            nn.Linear(self.embed_dim, 512), 
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(), 
-            nn.Linear(512, num_classes) if num_classes > 0 else nn.Identity())
-        #self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+        # self.fc_norm = norm_layer(embed_dim) if use_fc_norm else nn.Identity()
+        # self.head = nn.Sequential(
+        #     nn.Linear(self.embed_dim, 512), 
+        #     nn.ReLU(),
+        #     nn.Linear(512, 512),
+        #     nn.ReLU(), 
+        #     nn.Linear(512, num_classes) if num_classes > 0 else nn.Identity())
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
         if weight_init != 'skip':
             self.init_weights(weight_init)
